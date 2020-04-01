83996b8d04902ca1059e2e498947ce4f0587c7c0
Abdul Khan <akhan17@outlook.com>
2020-04-01 17:53:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+  def name 
+    @name
+  end
+  def breeds
+    @breeds
+  end
   
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  def breeds =(new_breed)
+    @breeds = new_breed
+  end
 end
