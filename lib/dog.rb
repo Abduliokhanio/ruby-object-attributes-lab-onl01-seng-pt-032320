84f91cc74f26264f80f4c271abe27877f5639a98
Abdul Khan <akhan17@outlook.com>
2020-04-01 17:55:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,15 @@ class Dog
   def name 
     @name
   end
-  def breeds
-    @breeds
+  def breed
+    @breed
   end
   
   
   def name=(new_name)
     @name = new_name
   end
-  def breeds=(new_breed)
-    @breeds = new_breed
+  def breed=(new_breed)
+    @breed = new_breed
   end
 end
